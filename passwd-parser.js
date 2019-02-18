4110510a2ee7ac0b8f9fc620d0d7ab750bdea729
maitreyaatish <maitreyaatish@gmail.com>
2019-02-17 19:01:36 -0800

    changes made

--- passwd-parser.js
+++ passwd-parser.js
@@ -6,9 +6,9 @@ if (!fs.existsSync('/etc/passwd') || !fs.existsSync('/etc/group')) {
     console.log("files do not exsist")
     throw new Error('Files do not exsist.Please change the arguments to point to the /etc/passwd and /etc/group files');
 }
-let a = parse(fs.readFileSync('/etc/passwd', 'utf8'))  //Parsing passwd file
-let b = parse(fs.readFileSync('/etc/group', 'utf8'))  //Parsing group file with the same module for simplicity of the code. The key's will be different in the output
-a.forEach(function(n){ // Creating intermediate subarrays and pusing them into the main array that will be returned
+let passwd_file_contents = parse(fs.readFileSync('/etc/passwd', 'utf8'))  //Parsing passwd file
+let group_file_contents = parse(fs.readFileSync('/etc/group', 'utf8'))  //Parsing group file with the same module for simplicity of the code. The key's will be different in the output
+passwd_file_contents.forEach(function(n){ // Creating intermediate subarrays and pusing them into the main array that will be returned
   let temp = {}
   temp['uid'] = n['uid']
   temp['full_name']= n['gecos']
@@ -16,7 +16,7 @@ a.forEach(function(n){ // Creating intermediate subarrays and pusing them into t
   gids1.set(n['gid'],n['username'])
   final[n['username']] = temp
 })
-b.forEach(function(n){ //Getting groups by matching group id's with those form the passwd file
+group_file_contents.forEach(function(n){ //Getting groups by matching group id's with those form the passwd file
   if(gids1.has(n['uid'])){ //here beacuse the parse-passwd module isn't made for the group file the mappings are different so you'll get gid in userid and henceforth
     let uname = gids1.get(n['uid'])
     final[uname]['groups'] = n['gid'].split(',')
